11cecb9b99e858aabba7a912a1caebbb8de5e2cc
wapl <33437769+wapl@users.noreply.github.com>
2019-11-27 00:39:07 +0800

    LOGIN UI DONE

---
+++ app/src/main/java/com/example/mobapde_mp/Login.java
@@ -0,0 +1,46 @@
+package com.example.mobapde_mp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+public class Login extends AppCompatActivity {
+
+    EditText et_username, et_password;
+    Button btn_login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        login();
+    }
+
+    void login(){
+        et_username = (EditText)findViewById(R.id.et_username);
+        et_password = (EditText)findViewById(R.id.et_password);
+        btn_login = (Button)findViewById(R.id.btn_login);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(et_username.getText().toString().equals("admin") && et_password.getText().toString().equals("admin")){
+                    Toast.makeText(Login.this, "Username and Password is correct", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent("com.razormist.simpleloginapplication.User");
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(Login.this, "Username or Password is incorrect", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
