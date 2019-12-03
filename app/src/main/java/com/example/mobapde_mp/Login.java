294b6f45d02c94285231b9173ead6f00c4ded9c8
wapl <33437769+wapl@users.noreply.github.com>
2019-12-03 10:38:37 +0800

    Able to datascrape Movie Details

--- app/src/main/java/com/example/mobapde_mp/Login.java
+++ app/src/main/java/com/example/mobapde_mp/Login.java
@@ -2,15 +2,12 @@ package com.example.mobapde_mp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-
 
 public class Login extends AppCompatActivity {
 
---
+++ app/src/main/java/com/example/mobapde_mp/MovieDetails.java
@@ -0,0 +1,53 @@
+package com.example.mobapde_mp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.androdocs.httprequest.HttpRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MovieDetails extends AppCompatActivity {
+    TextView txtTitle;
+    TextView txtPlot;
+    String Title="Mr+Bean";
+    String api="48adfd0c";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_details);
+        txtTitle=findViewById(R.id.txtTitle);
+        txtPlot=findViewById(R.id.txtPlot);
+
+
+        new movieTask().execute();
+    }
+
+    class movieTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+            String response= HttpRequest.excuteGet("http://www.omdbapi.com/?t=" + Title + "&plot=full&apikey=" + api);
+            return response;
+        }
+        @Override
+        protected void onPostExecute(String result){
+            try {
+                JSONObject jsonObj = new JSONObject(result);
+                String plot= jsonObj.getString("Plot");
+                String title=jsonObj.getString("Title");
+                txtTitle.setText(title);
+                txtPlot.setText(plot);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+
+}
